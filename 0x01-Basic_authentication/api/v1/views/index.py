f72434a8548513695568160e8f22acb97c3f086a
lipnganga <philipmurage98@gmail.com>
2023-06-01 23:20:45 +0300

    solution to task 1

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,26 +4,28 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
-# unauthorised access routes
-@app_views.route('/unauthorized/', method=['GET'], strict_slashes=False)
+
+# unauthorized access
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    Return:
-      - abort(401)
+    """_summary_
+
+    Returns:
+        str: _description_
     """
-    abort(401, description="Unauthorized")
+    abort(401, description='Unauthorized')
 
-
-    # forbidden access routes
-@app_views.route('/forbidden/', method=['GET'], strict_slashes=False)
+# forbidden
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
-    """ GET /api/v1/forbidden
-    Return:
-      - abort(403)
-    """
-    abort(403, description="Forbidden")
+    """_summary_
 
-    
+    Returns:
+        str: _description_
+    """
+    abort(403, description='Forbidden')
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
