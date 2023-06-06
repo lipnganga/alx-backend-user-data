e5c119c225b08cd8e01d53c5d6a267a5a21a3568
lipnganga <philipmurage98@gmail.com>
2023-06-06 22:19:10 +0300

    first commit

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # create a user model
 
-from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class User(Base):
+    """summary: create a user model"""
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
