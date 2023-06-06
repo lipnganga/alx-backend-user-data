9b7d0e48ac79c17f75fa25f459664f115fb654ae
lipnganga <philipmurage98@gmail.com>
2023-06-06 19:30:59 +0300

    test out module

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base
+from user import User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+    
+    def add_user(self, email: str, hashed_password: str) -> None:
+        """Method that saves a new user to the database
+        """
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return None
+    
+    def find_user_by(self, **kwargs) -> User:
+        """Method that returns the first row found in the users table
+        as filtered by the method’s input arguments
+        """
+        return self._session.query(User).filter_by(**kwargs).first()
+    
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Method that takes as argument a required user_id integer
+        and arbitrary keyword arguments, and returns None
+        """
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if not hasattr(user, key):
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
+        return None
+    
