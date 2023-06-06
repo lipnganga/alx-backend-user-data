dd4151fc349e6778a75cf01ede3729084edb85aa
lipnganga <philipmurage98@gmail.com>
2023-06-06 22:16:16 +0300

    iniial commit

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -14,6 +14,6 @@ class User(Base):
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-    
+
     def __repr__(self):
         return "<User: %s>" % self.username
