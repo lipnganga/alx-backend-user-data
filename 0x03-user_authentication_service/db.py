334db023109942a4e729ce12f51ed0392aff18b3
lipnganga <philipmurage98@gmail.com>
2023-06-06 22:31:39 +0300

    add sql clas

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
-from user import Base
-from user import User
+from user import Base, User
 
 
 class DB:
@@ -17,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -30,30 +31,41 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
-    def add_user(self, email: str, hashed_password: str) -> None:
-        """Method that saves a new user to the database
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """_summary_
         """
         new_user = User(email=email, hashed_password=hashed_password)
+        # add new user and commit to database
         self._session.add(new_user)
         self._session.commit()
-        return None
-    
+        return new_user
+
     def find_user_by(self, **kwargs) -> User:
-        """Method that returns the first row found in the users table
-        as filtered by the method’s input arguments
+        """_summary_
+
+        Returns:
+            User: _description_
         """
-        return self._session.query(User).filter_by(**kwargs).first()
-    
+        if not kwargs:
+            raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
+
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Method that takes as argument a required user_id integer
-        and arbitrary keyword arguments, and returns None
+        """_summary_
+
+        Args:
+            user_id (int): _description_
         """
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
             if not hasattr(user, key):
                 raise ValueError
             setattr(user, key, value)
+
         self._session.commit()
         return None
-    
